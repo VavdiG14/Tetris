302733c4b15c3807fd2129d980e38b67c9774f06
VavdiG14 <greg.vudi@gmail.com>
2017-04-11 10:19:45 +0200

    Second commit

--- init.py
+++ init.py
@@ -1,2 +1,28 @@
 ###Uvod v programiranje igrice Tetris
 
+from tkinter import*
+
+#definiramo širino kvadratka, ki nam bo služil kot merilo za velikost elementov.
+k = 20
+
+class GUI():
+
+    def __init__(self, master):
+        self.platno = Canvas(master, width=15*k, height=20*k, background='black')
+        self.platno.pack(side=RIGHT)
+        menu = Menu(master)
+        master.config(menu=menu)
+        f = Frame(master)
+        f.pack()
+        pravokotnik = self.platno.create_rectangle(100,100, 100+k, 200-k, fill = 'blue')
+        pravokotnik_r1 = self.platno.create_rectangle(150, 100, 150-k, 100+k, fill='green')
+
+
+
+
+
+
+
+root = Tk()
+app=GUI(root)
+root.mainloop()
