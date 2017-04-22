d0eade1b667911c3eb000543dc8311b9e597601e
VavdiG14 <greg.vudi@gmail.com>
2017-04-22 16:02:28 +0200

    Postavljene osnove za premikanje in rotiranje

--- init.py
+++ init.py
@@ -16,65 +16,95 @@ class Lik():
     rotirali dani objekt..'''
 
     def __init__(self, igrica,imeLika):
-        self.oblika = [kocke[imeLika]]#dobi seznam vseh oblik lika
-        self.rotacija = 0   #privzeta trenutna rotacija
+        self.min_x = zacetekx
+        self.max_x = zacetekx
+        self.dno = dno
+        self.oblika = kocke[imeLika]#dobi seznam vseh oblik lika
+        self.rotacija = 1   #privzeta trenutna rotacija
+        self.x = zacetekx
+        self.y = zaceteky
+        self.koord = []
         self.gids = []
         self.igrica = igrica
         self.narisiLik()
         self.igrica.platno.bind("<Left>", self.premikvLevo)
         self.igrica.platno.bind("<Right>", self.premikvDesno)
-        self.igrica.platno.bind("<space>", self.rotiraj)
+        self.igrica.platno.bind("<space>", self.rotiranje)
+        self.padanje_kocke()
 
     def narisiLik(self):
-        for (x,y) in self.oblika[self.rotacija%len(self.oblika)]:
+        stevec = 0
+        for (u,v) in self.oblika[self.rotacija % len(self.oblika)]:
             id = self.igrica.platno.create_rectangle(
-                zacetekx +(x * k), zaceteky +(y*k), zacetekx +(x+1)*k, zaceteky + (y+1) * k, fill ='pink')
-        self.gids.append(id)
+                self.x +(u * k), self.y +(v*k), self.x +(u+1)*k, self.y + (v+1) * k, fill ='pink')
+            self.koord.append([(self.x +(u * k), self.y +(v*k)), (self.x +(u+1)*k, self.y + (v+1) * k)])
+            self.gids.append(id)
 
-    def osvezi(self):
-        pass
 
-    def rotiraj(self, events):
+    def rotiranje(self, events):
         self.rotacija += 1
-        self.narisiLik()
+        r = []
+        i  =0
+        for (u, v) in self.oblika[self.rotacija % len(self.oblika)]:
+                self.igrica.platno.coords(self.gids[i], self.x +(u * k), self.y +(v*k), self.x +(u+1)*k, self.y + (v+1) * k)
+                r += [self.x +(u * k), self.x +(u+1)*k]
+                self.koord[i] = [(self.x +(u * k), self.y +(v*k)), (self.x +(u+1)*k, self.y + (v+1) * k)]
+                i+= 1
+        self.min_x = min(r)
+        self.max_x = max(r)
+        print(self.min_x, self.max_x)
+        #Tudi pri rotacijah moraš preveriti, ali se lahko sploh zarotira.
         #Preveri, ali se ga da narisati, glede na druge postavitve
-        self.osvezi()
 
 
     def padanje_kocke(self,level = 3):
         '''Kocka se spušča proti dnu'''
-        self.igrica.platno.move(self.koc1, 0, k)
-        self.igrica.platno.move(self.koc2, 0, k)
-        self.igrica.platno.move(self.koc3, 0, k)
-        self.igrica.platno.move(self.koc4, 0, k)
-        #Tukaj bo moral gledati ali se je že zaletel v prejšno kocko/dno
-        if self.dno == 0:
-            print('Zdj se moreš ustavit')
-            return 0
-        else:
-            self.dno -= 1
-            self.igrica.platno.after(1000, self.padanje_kocke)
+        self.igrica.platno.update()
+        for i in self.gids:
+            self.igrica.platno.move(i, 0, k)
+        self.y += k
+
+        self.igrica.platno.after(1000, self.padanje_kocke)
 
 
     def premikvLevo(self,event):
         '''Premikanje Objekta v levo'''
-        #TODO: omeji njegovo gibanje glede na zaslon
-        self.igrica.platno.move(self.koc1, -k, 0)
-        self.igrica.platno.move(self.koc2, -k, 0)
-        self.igrica.platno.move(self.koc3, -k, 0)
-        self.igrica.platno.move(self.koc4, -k, 0)
+        self.min_x += -k
+        if self.preveriPremik():
+            for u in self.gids:
+                self.igrica.platno.move(u, -k, 0)
+            self.x -= k
+            self.max_x += -k
+        else:
+            self.min_x += k
+
         self.igrica.platno.update()
 
     def premikvDesno(self,event):
         '''Premikanje Objekta v desno'''
-        #TODO: omeji njegovo gibanje glede na zaslon
-        self.igrica.platno.move(self.koc1, k, 0)
-        self.igrica.platno.move(self.koc2, k, 0)
-        self.igrica.platno.move(self.koc3, k, 0)
-        self.igrica.platno.move(self.koc4, k, 0)
+
+        self.max_x += k
+        print(self.min_x, self.max_x)
+        if self.preveriPremik():
+            for u in self.gids:
+                self.igrica.platno.move(u, k, 0)
+            self.x += k
+            self.min_x += k
+        else:
+            self.max_x += -k
+
         self.igrica.platno.update()
 
-
+    def preveriPremik(self):
+        '''Metoda preveri ali je premik/rotacija dovoljena.
+            Metoda vrača True/False'''
+        #LEVO
+        if self.min_x < 0:
+            return False
+        elif self.max_x > 15*k:
+            return False
+        else:
+            return True
 
 class GUI():
     '''Tukaj bo definiran uporabniški vmesnik'''
@@ -85,7 +115,7 @@ class GUI():
         menu = Menu(master)
         master.config(menu=menu)
         self.platno.focus_set()
-        self.kocka = Lik(self,'palcka')
+        self.kocka = Lik(self,'kockaT')
 
 
 
