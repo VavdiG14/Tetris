8745e7b39d21c8c715d1d810f4ecba1288957082
VavdiG14 <greg.vudi@gmail.com>
2017-04-25 12:14:37 +0200

    Skoraj dokončano.
    Sistem za kočanje igre in začetek nove igre.
    Level in točke.

--- init.py
+++ init.py
@@ -2,28 +2,28 @@
 
 from tkinter import*
 from tabelakock import*
-import time
+import random
+from tkinter import messagebox
+
 #definiramo širino kvadratka, ki nam bo služil kot merilo za velikost elementov.
 k = 30
-zacetekx=k*15/2
+zacetekx=7
 zaceteky=0
 dno = 17
-vse_kocke=kocke.keys()
+vse_kocke=list(kocke.keys())
 
 class Lik():
     '''V nadaljevanju naj bi bil to class Objekt s katerim bi
      premikali in
     rotirali dani objekt..'''
 
-    def __init__(self, igrica,imeLika):
-        self.min_x = zacetekx
-        self.max_x = zacetekx
-        self.dno = dno
+    def __init__(self, igrica,imeLika,barva,koord):
+        self.barva = barva
         self.oblika = kocke[imeLika]#dobi seznam vseh oblik lika
-        self.rotacija = 1   #privzeta trenutna rotacija
+        self.rotacija = 0   #privzeta trenutna rotacija
         self.x = zacetekx
         self.y = zaceteky
-        self.koord = []
+        self.koord = koord
         self.gids = []
         self.igrica = igrica
         self.narisiLik()
@@ -33,78 +33,89 @@ class Lik():
         self.padanje_kocke()
 
     def narisiLik(self):
-        stevec = 0
-        for (u,v) in self.oblika[self.rotacija % len(self.oblika)]:
-            id = self.igrica.platno.create_rectangle(
-                self.x +(u * k), self.y +(v*k), self.x +(u+1)*k, self.y + (v+1) * k, fill ='pink')
-            self.koord.append([(self.x +(u * k), self.y +(v*k)), (self.x +(u+1)*k, self.y + (v+1) * k)])
-            self.gids.append(id)
+        if self.koord[0][7] is not None:
+            messagebox.showinfo("Koncaj igro", "This is a Test")
+        else:
+            for (u,v) in self.oblika[self.rotacija % len(self.oblika)]:
+                id = self.igrica.platno.create_rectangle(
+                    self.x * k +(u * k), self.y*k +(v*k), self.x*k +(u+1)*k, self.y* k + (v+1) * k, fill = self.barva)
+                self.gids.append(id)
+
 
 
     def rotiranje(self, events):
-        self.rotacija += 1
-        r = []
-        i  =0
-        for (u, v) in self.oblika[self.rotacija % len(self.oblika)]:
-                self.igrica.platno.coords(self.gids[i], self.x +(u * k), self.y +(v*k), self.x +(u+1)*k, self.y + (v+1) * k)
-                r += [self.x +(u * k), self.x +(u+1)*k]
-                self.koord[i] = [(self.x +(u * k), self.y +(v*k)), (self.x +(u+1)*k, self.y + (v+1) * k)]
-                i+= 1
-        self.min_x = min(r)
-        self.max_x = max(r)
-        print(self.min_x, self.max_x)
-        #Tudi pri rotacijah moraš preveriti, ali se lahko sploh zarotira.
-        #Preveri, ali se ga da narisati, glede na druge postavitve
-
+        oren = (self.rotacija + 1 )% len(self.oblika)
+        if self.preveriPremik(oren, self.x, self.y):
+            self.rotacija = oren
+            i = 0
+            for (u, v) in self.oblika[oren]:
+                    self.igrica.platno.coords(self.gids[i], self.x*k +(u * k), self.y*k +(v*k), self.x*k +(u+1)*k,
+                                              self.y*k + (v+1) * k)
+                    i += 1
 
     def padanje_kocke(self,level = 3):
         '''Kocka se spušča proti dnu'''
         self.igrica.platno.update()
-        for i in self.gids:
-            self.igrica.platno.move(i, 0, k)
-        self.y += k
-
-        self.igrica.platno.after(1000, self.padanje_kocke)
-
+        if self.preveriPremik(self.rotacija, self.x, self.y+1):
+            for i in self.gids:
+                self.igrica.platno.move(i, 0, k)
+            self.y += 1
+            self.igrica.platno.after(250, self.padanje_kocke)
+        else:
+            self.osveziTabelo(self.rotacija, self.x, self.y)
+            self.igrica.naslednjiLik(self.koord)
 
     def premikvLevo(self,event):
         '''Premikanje Objekta v levo'''
-        self.min_x += -k
-        if self.preveriPremik():
+        if self.preveriPremik(self.rotacija, self.x -1, self.y):
             for u in self.gids:
                 self.igrica.platno.move(u, -k, 0)
-            self.x -= k
-            self.max_x += -k
-        else:
-            self.min_x += k
-
+            self.x -= 1
         self.igrica.platno.update()
 
     def premikvDesno(self,event):
         '''Premikanje Objekta v desno'''
-
-        self.max_x += k
-        print(self.min_x, self.max_x)
-        if self.preveriPremik():
+        if self.preveriPremik(self.rotacija, self.x+1, self.y):
             for u in self.gids:
                 self.igrica.platno.move(u, k, 0)
-            self.x += k
-            self.min_x += k
-        else:
-            self.max_x += -k
+            self.x += 1
 
         self.igrica.platno.update()
 
-    def preveriPremik(self):
+    def preveriPremik(self,orientacija, x,y):
         '''Metoda preveri ali je premik/rotacija dovoljena.
             Metoda vrača True/False'''
-        #LEVO
-        if self.min_x < 0:
-            return False
-        elif self.max_x > 15*k:
-            return False
-        else:
-            return True
+        for (u,v) in self.oblika[orientacija]:
+            noviX = u + x
+            noviY = v + y
+            if noviY >= 20:     #če pade na tla
+                return False
+            if noviX < 0 or noviX >= 15:        #če se zaleti levo ali desno
+                return False
+            if self.koord[noviY][noviX] is not None:
+
+                return False
+        return True
+
+    def osveziTabelo(self,orientacija, x,y):
+        i = 0
+        for (u, v) in self.oblika[orientacija]:
+            novX = u + x
+            novY = v + y
+            (self.koord[novY])[novX] = self.gids[i]
+            i += 1
+        for st,vrstica in enumerate(self.koord):
+            if None not in vrstica:
+                for gid in vrstica:
+                    self.igrica.platno.delete(gid)
+                for st2 in range(st+1):
+                    for gid in self.koord[st2]:
+                            if gid is not None:
+                                self.igrica.platno.move(gid, 0, k)
+                self.koord.remove(vrstica)
+                self.koord = [[None for i in range(16)]]+ self.koord
+
+
 
 class GUI():
     '''Tukaj bo definiran uporabniški vmesnik'''
@@ -114,12 +125,19 @@ class GUI():
         self.platno.pack(side=RIGHT)
         menu = Menu(master)
         master.config(menu=menu)
+        menu.add_command(label="Nova igra", command=self.novaIgra)
+        menu.add_command(label="Koncaj", command=master.destroy)
         self.platno.focus_set()
-        self.kocka = Lik(self,'kockaT')
 
 
 
+    def naslednjiLik(self,koord=[[None for i in range(15)] for j in range(20)]):
+        barve = ['green', 'blue', 'red', 'pink']
+        nakljucnaBarva = random.choice(barve)
+        return Lik(self, random.choice(vse_kocke), nakljucnaBarva,koord)
 
+    def novaIgra(self):
+        self.kocka = self.naslednjiLik()
 
 
 root = Tk()
